aac600939c4e576082db8186656894ea2c0557e3
Applex <alex.nantong@gmail.com>
2015-06-15 21:09:07 +0800

    update api.

--- src/main/java/cn/edu/sjtu/se/dclab/auth/thrift/AuthClient.java
+++ src/main/java/cn/edu/sjtu/se/dclab/auth/thrift/AuthClient.java
@@ -21,7 +21,14 @@ public class AuthClient {
 	private Auth.Client client;
 	private String nodeName;
 	
-	
+	public String getNodeName() {
+		return nodeName;
+	}
+
+	public void setNodeName(String nodeName) {
+		this.nodeName = nodeName;
+	}
+
 	public void startClient() throws Exception {
 		ServiceManager manager = ServiceManager.getInstance();
 		List<String> contents = manager.retrieve(nodeName);
@@ -36,6 +43,8 @@ public class AuthClient {
 		
 		serverIp = parts[0];
 		port = Integer.parseInt(parts[1]);
+		
+		System.out.println("serverip " + serverIp);
 	}
 	
 	public Boolean validation(int from, int to, int type) {
@@ -56,7 +65,7 @@ public class AuthClient {
 			 
 			JSONObject jsonObject = new JSONObject(map);
 			String operation = jsonObject.toString();
-			Boolean result = client.hasAuthority(0, operation);
+			Boolean result = client.hasAuthority("", operation);
 			System.out.println("Return from server: " + result);
 			transport.close();
 			return result;
@@ -66,16 +75,67 @@ public class AuthClient {
 		}
 
 	}
+	
+    public boolean verifyDevice(String dtToken) {
+    	System.out.println("Verify Device by token: ");
+		try {
+			TSocket transport = new TSocket(serverIp, port);
+			transport.open();
+			TProtocol protocol = new TBinaryProtocol(transport);
 
+			Auth.Client client = new Auth.Client(protocol);
 
-	public String getNodeName() {
-		return nodeName;
-	}
+			Boolean result = client.deviceTrustable(dtToken);
+			
+			System.out.println("Return from server: " + result);
+			transport.close();
+			return result;
+		} catch (TException e) {
+			e.printStackTrace();
+			return false;
+		}
+    }
 
-	public void setNodeName(String nodeName) {
-		this.nodeName = nodeName;
-	}
+    public String verifyDevice(String publicKey, String encryptedString, String trueString) {
+    	System.out.println("Verify Device by publicKey");
+		try {
+			TSocket transport = new TSocket(serverIp, port);
+			transport.open();
+			TProtocol protocol = new TBinaryProtocol(transport);
 
+			Auth.Client client = new Auth.Client(protocol);
+
+			String token = client.verifyDevice(publicKey, encryptedString, trueString);
+			
+			System.out.println("Return from server: " + token);
+			transport.close();
+			return token;
+		} catch (TException e) {
+			e.printStackTrace();
+			return "";
+		}
+    }
+    
+    private static AuthClient singleton = null;
+    private AuthClient() {
+    	nodeName = "/authService";
+		try {
+			startClient();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return;
+		}
+    }
+    
+    public static AuthClient getInstance() {
+    	if (singleton == null) {
+    		singleton = new AuthClient();
+    	}
+		return singleton;
+    }
+    
+    /*
 	public static void main(String[] args) {
 		AuthClient client = new AuthClient();
 		client.setNodeName("/authService");
@@ -88,5 +148,5 @@ public class AuthClient {
 		}
 		System.out.println(client.validation(1, 1, 1));
 	}
-
+	*/
 }
